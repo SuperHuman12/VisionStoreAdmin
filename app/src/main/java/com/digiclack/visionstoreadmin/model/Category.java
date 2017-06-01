19753f77f218ff4914e34fe0b847a74a63c7489a
ZarSaeed <zarafridi1@live.com>
2017-06-01 14:13:53 +0500

    further added

---
+++ app/src/main/java/com/digiclack/visionstoreadmin/model/Category.java
@@ -0,0 +1,26 @@
+package com.digiclack.visionstoreadmin.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Zar on 6/1/2017.
+ */
+
+public class Category {
+    private ArrayList<Product> products;
+    private String cName;
+
+    public Category() {}
+    public Category(ArrayList<Product> products,String cName) {
+        this.cName=cName;
+        this.products=products;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+}
